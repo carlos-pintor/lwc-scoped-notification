585e2af9491d7556c8e96f2fa8665a5322da88ac
Carlos Pintor <36550078+carlos-pintor@users.noreply.github.com>
2020-12-03 11:36:50 -0600

    Use slots to get markup from the parent component

--- force-app/main/default/lwc/scoped_notification/scoped_notification.js
+++ force-app/main/default/lwc/scoped_notification/scoped_notification.js
@@ -4,10 +4,6 @@ const notificationBaseCSS = "slds-scoped-notification slds-media slds-media_cent
 
 export default class Scoped_notification extends LightningElement {
     @api themeCss = "";
-    @api iconName = "";
-    @api iconSize = "medium";
-    @api iconTitle = "";
-    @api bodyMessage = "";
 
     get notificationCSS() {
         return notificationBaseCSS + " " + this.themeCss;
